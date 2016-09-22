d0d01496f6d47d6ec509e713444909b606ee0a2e
somename <someemail>
2016-09-22 16:23:44 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -7,31 +7,41 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_getfirstthrow_7() {
+		Frame frame = new Frame(7,0);
+		assertEquals("The first throw is not correctly displayed, 7 intended", 7, frame.getFirstThrow());
 	}
-
+	
+	@Test
+	public void test_getsecondthrow_4() {
+		Frame frame = new Frame(5,4);
+		assertEquals("this second throw is not correctly displayed, 4 intended",4, frame.getSecondThrow());
+	}
 	
 	@Test
 	public void Score_firstthrow1_secondthrow4() {;
 		Frame frame =new Frame(1, 4);
 		assertEquals("The score is not correct, expected 5", 5, frame.score());	
 	}
+	
 	@Test
 	public void Spare_istrue() {
 		Frame frame = new Frame(5,5);
 		assertEquals("The spare is not correctly detected", true, frame.isSpare());
 	}
+	
 	@Test
 	public void Spare_isFalse() {
 		Frame frame = new Frame(4,5);
 		assertEquals("The shot is not spare but get detected as one", false, frame.isSpare());
 	}
+	
 	@Test
 	public void Strike_istrue() {
 		Frame frame = new Frame(10,0);
 		assertEquals("The strike is not correctly detected", true, frame.isStrike());
 	}
+	
 	@Test
 	public void Strike_isFalse() {
 		Frame frame = new Frame(4,1);
@@ -42,20 +52,16 @@ public class TestBowling {
 	public void add_frame(){
 		 BowlingGame BowlingGame = new BowlingGame();
 		 BowlingGame.addFrame(new Frame(1,2));
-		 assertEquals("The frame wasn't implemented right",3,BowlingGame.score());
-		 
+		 assertEquals("The frame wasn't implemented right",3,BowlingGame.score());	 
 	 }
 		 
-	 
-	
 	@Test
 	public void add_2frames(){
 		BowlingGame BowlingGame = new BowlingGame();
 		
 		BowlingGame.addFrame(new Frame(9,0));
 		BowlingGame.addFrame(new Frame(8,1));
-		assertEquals("The frames didn't get implemented correctly",18, BowlingGame.score());
-		
+		assertEquals("The frames didn't get implemented correctly",18, BowlingGame.score());	
 	}
 	
 	@Test
@@ -63,8 +69,7 @@ public class TestBowling {
 		BowlingGame BowlingGame= new BowlingGame();
 		BowlingGame.addFrame(new Frame(10,0));
 		BowlingGame.addFrame(new Frame(3,3));
-		assertEquals("The score is not correctly calculated, should be 22",22,BowlingGame.score());
-		
+		assertEquals("The score is not correctly calculated, should be 22",22,BowlingGame.score());	
 	}
 	
 	@Test
@@ -81,7 +86,8 @@ public class TestBowling {
 		BowlingGame.addFrame(new Frame(0,0));//8
 		BowlingGame.addFrame(new Frame(10,0));//9
 		BowlingGame.setBonus(3,3);//bonus
-		assertEquals("The score is not correctly calculated, should be 16",16,BowlingGame.score());}
+		assertEquals("The score is not correctly calculated, should be 16",16,BowlingGame.score());
+	}
 	
 	@Test
 	public void bonus_score_spare_16(){
@@ -97,7 +103,8 @@ public class TestBowling {
 		BowlingGame.addFrame(new Frame(0,0));//8
 		BowlingGame.addFrame(new Frame(9,1));//9
 		BowlingGame.setBonus(6,3);//bonus
-		assertEquals("The score is not correctly calculated, should be 16",16,BowlingGame.score());}
+		assertEquals("The score is not correctly calculated, should be 16",16,BowlingGame.score());
+	}
 	
 
 
@@ -115,7 +122,9 @@ public class TestBowling {
 		BowlingGame.addFrame(new Frame(10,0));//8
 		BowlingGame.addFrame(new Frame(2,8));//9
 		BowlingGame.setBonus(6,0);//bonus
-		assertEquals("The score is not correctly calculated, should be 133",133,BowlingGame.score());}}
+		assertEquals("The score is not correctly calculated, should be 133",133,BowlingGame.score());
+	}
+}
 
 
 	
